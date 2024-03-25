9aefad9f338e15a180fc4773eb6e90361b17580a
zawarudobngdev <murilo.m@hotmail.com>
2024-03-25 19:36:35 -0300

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import {useEffect, useState} from 'react'
+import ContactList from "./ContactList";
+import './App.css'
+import ContactForm from "./ContactForm.jsx";
+
+function App() {
+    const [contacts, setContacts] = useState([])
+    const [isModalOpen, setIsModelOpen] = useState(false)
+    const [currentContact, setCurrentContact] = useState({})
+
+    useEffect(() => {
+        fetchContacts()
+    }, []);
+
+    const fetchContacts = async () => {
+        const response = await fetch("http://127.0.0.1:5000/contacts")
+        const data = await response.json()
+        setContacts(data.contacts)
+    };
+
+    const closeModal = () => {
+        setIsModelOpen(false)
+        setCurrentContact({})
+    }
+
+    const openCreateModal = () => {
+        if (!isModalOpen) setIsModelOpen(true)
+    }
+
+    const openEditModal = (contact) => {
+        if (isModalOpen) return
+        setCurrentContact(contact)
+        setIsModelOpen(true)
+    }
+
+    const onUpdate = () => {
+        closeModal()
+        fetchContacts()
+    }
+
+    return (
+        <>
+            <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate}/>
+            <button onClick={openCreateModal}>Create New Contact</button>
+            {isModalOpen && <div className="modal">
+                <div className="modal-content">
+                    <span className="close" onClick={closeModal}>&times;</span>
+                    <ContactForm existingContact={currentContact} updateCallback={onUpdate}/>
+                </div>
+            </div>
+            }
+        </>
+    );
+}
+
+export default App
